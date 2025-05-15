5f5584c3ab660b45f4038d0b1f556fca81c90f75
Orinda <orindatt@gmail.com>
2025-05-15 14:16:40 +0200

    Exclude terraform folder from Git tracking

--- terraform/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "app_repo" {
-  name                 = "${var.project_name}-ecr"
-  image_tag_mutability = "MUTABLE"
-
-  tags = {
-    Name = "${var.project_name}-ecr"
-  }
-}
-
